001a5156f9c96578a35f2762ef265143bd60bc33
Hammy Havoc <hammy@previousmagazine.com>
2016-07-29 07:45:32 +0100

    Updated UX.

--- config.js
+++ config.js
@@ -5,12 +5,16 @@ define( function ( require ) {
 	return {
 		app_slug : 'the-boo-tube-for-android',
 		wp_ws_url : 'https://www.thebootube.com/wp-appkit-api/the-boo-tube-for-android',
-		theme : 'wpak-tabs-android',
-		version : '1.0',
+		wp_url : 'https://www.thebootube.com',
+		theme : 'q-android',
+		version : '2.0',
 		app_title : 'The Boo Tube',
+		app_platform : 'android',
+		gmt_offset : 1,
 		debug_mode : 'off',
 		auth_key : '<,5tjZ!AZ.S&<],_+y{ue[S#@A[AZk41^.S-VK4~wJx%9<L0</ro9Alw(Ho{r!{%',
 		options : {"refresh_interval":0},
+		theme_settings : [],
 		addons : []
 	};
 
